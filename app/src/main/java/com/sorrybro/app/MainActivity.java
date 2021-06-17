f7ed0f52cd44cc3f393ade0db0abf1a83652e538
althafvly <althafvly@gmail.com>
2021-06-17 19:50:19 +0530

    Add functions for image fetch and view
    add warnings for no internet

--- app/src/main/java/com/sorrybro/app/MainActivity.java
+++ app/src/main/java/com/sorrybro/app/MainActivity.java
@@ -1,14 +1,15 @@
 package com.sorrybro.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.drawable.Drawable;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,18 +18,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imgView = findViewById(R.id.imageView);
-        Drawable drawable = LoadImageFromWebOperations("https://picsum.photos/200/300");
-        imgView.setImageDrawable(drawable);
+        int loader = R.drawable.dummy;
+        ImageView image = findViewById(R.id.image);
+        String image_url = "https://picsum.photos/720";
+        ImageLoader imgLoader = new ImageLoader(getApplicationContext());
+        imgLoader.DisplayImage(image_url, loader, image);
+        ShowWarning();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            ShowWarning();
+            imgLoader.clearCache();
+            imgLoader.DisplayImage(image_url, loader, image);
+        });
     }
-    private Drawable LoadImageFromWebOperations(String url)
-    {
-            try {
-                InputStream is = (InputStream) new URL(url).getContent();
-                return Drawable.createFromStream(is, "src name");
-            } catch (Exception e) {
-                System.out.println("Exc="+e);
-                return null;
-            }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void ShowWarning() {
+        TextView textimage = findViewById(R.id.textView);
+        if(!isNetworkAvailable()) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    R.string.no_network_error,
+                    Toast.LENGTH_SHORT);
+            toast.show();
+            textimage.setText(R.string.old_image_warning);
+        } else {
+            textimage.setText("");
+        }
     }
 }
