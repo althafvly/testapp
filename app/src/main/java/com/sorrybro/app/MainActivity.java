9e9c34545062e5d69d575791369ee48127fa643c
althafvly <althafvly@gmail.com>
2021-06-17 22:56:49 +0530

    Fix image view

--- app/src/main/java/com/sorrybro/app/MainActivity.java
+++ app/src/main/java/com/sorrybro/app/MainActivity.java
@@ -19,26 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final File cacheDir = new File(android.os.Environment.getExternalStorageDirectory(),"TestAppImages");
 
         int loader = R.drawable.dummy;
         ImageView image = findViewById(R.id.image);
         String image_url = "https://picsum.photos/720";
         ImageLoader imgLoader = new ImageLoader(getApplicationContext());
 
-        if(cacheDir.exists()) {
-            String[] files = cacheDir.list();
-            if (files.length != 0) {
-                imgLoader.DisplayImage(image_url, loader, image);
-            }
+        if(!isDirectoryEmpty()) {
+            imgLoader.DisplayImage(image_url, loader, image);
+            ShowImageWarning();
         }
 
-        ShowWarning();
+        ShowNetworkWarning();
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(v -> {
-            ShowWarning();
-            imgLoader.clearCache();
+            ShowNetworkWarning();
+            ShowImageWarning();
+            if(isNetworkAvailable()) {
+                imgLoader.clearCache();
+            }
             imgLoader.DisplayImage(image_url, loader, image);
         });
     }
@@ -50,16 +50,30 @@ public class MainActivity extends AppCompatActivity {
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
-    private void ShowWarning() {
+    private void ShowImageWarning() {
         TextView textimage = findViewById(R.id.textView);
+        if(!isNetworkAvailable()) {
+            textimage.setText(R.string.old_image_warning);
+        } else {
+            textimage.setText("");
+        }
+    }
+
+    private void ShowNetworkWarning() {
         if(!isNetworkAvailable()) {
             Toast toast = Toast.makeText(getApplicationContext(),
                     R.string.no_network_error,
                     Toast.LENGTH_SHORT);
             toast.show();
-            textimage.setText(R.string.old_image_warning);
-        } else {
-            textimage.setText("");
         }
+    }
+
+    private boolean isDirectoryEmpty() {
+        final File cacheDir = new File(android.os.Environment.getExternalStorageDirectory(),"TestAppImages");
+        if(cacheDir.exists()) {
+            String[] files = cacheDir.list();
+            return files.length == 0;
+        }
+        return false;
     }
 }
